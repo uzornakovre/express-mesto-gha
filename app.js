ef19e65590c9afca49cba49a667b84d6fc82ea07
Konstantin Filyaev <uzornakovre@yandex.ru>
2023-02-19 13:46:32 +0300

    feat: user schema

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
-const express = require('express');
+const path            = require('path');
+const express         = require('express');
+const mongoose        = require('mongoose');
+const router          = require('express').Router();
+const { PORT = 3000 } = process.env;
 
 const app = express();
 
-app.listen(3000);
+mongoose.connect('mongodb://localhost:27017/mestodb')
+  .then(() => {
+    console.log('connected')
+  })
+
+router.get('/', (req, res) => {
+  console.log('asdasd');
+})
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+})
---
+++ models/user.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+  about: String,
+});
+
+module.exports = mongoose.model('user', userSchema);
