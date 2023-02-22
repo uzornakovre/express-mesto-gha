11d987f3b6188789ec45005ec2b1325e6ff95d79
Konstantin Filyaev <uzornakovre@yandex.ru>
2023-02-22 13:42:45 +0300

    feat: error validation update

--- routes/error.js
+++ routes/error.js
@@ -2,5 +2,9 @@ const router = require('express').Router();
 const { error } = require('../controllers/error');
 
 router.get('/*', error);
+router.post('/*', error);
+router.put('/*', error);
+router.patch('/*', error);
+router.delete('/*', error);
 
 module.exports = router;
