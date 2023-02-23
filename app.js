6c1b1ef38fc2f2fdd036026e25260778cf17193e
Konstantin Filyaev <uzornakovre@yandex.ru>
2023-02-24 02:27:06 +0300

    fix: body-parser uninstalled

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
 
 const { PORT = 3000 } = process.env;
 
@@ -8,8 +7,7 @@ const app = express();
 
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
 app.use((req, res, next) => {
   req.user = {
     _id: '63f247aa1f316ef793534526',
