a0b15baee868dc1dd28d152c8237b9973f5813dd
Konstantin Filyaev <uzornakovre@yandex.ru>
2023-03-09 00:51:35 +0300

    feat: routes added

--- controllers/users.js
+++ controllers/users.js
@@ -97,3 +97,24 @@ module.exports.updateUserAvatar = (req, res) => {
       }
     });
 };
+
+module.exports.login = (req, res) => {
+  const { email, password } = req.body;
+
+  User.findOne({ email })
+    .then((user) => {
+      if (!user) {
+        return Promise.reject(new Error('Неправильные почта или пароль'));
+      }
+      return bcrypt.compare(password, user.password);
+    })
+    .then((matched) => {
+      if (!matched) {
+        return Promise.reject(new Error('Неправильные почта или пароль'));
+      }
+      res.send({ message: 'Всё верно!' });
+    })
+    .catch((err) => {
+      res.status(401).send({ message: err.message });
+    });
+};
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
 
+router.use('/signup', require('./signup'));
+router.use('/signin', require('./signin'));
 router.use('/users', require('./users'));
 router.use('/cards', require('./cards'));
 router.use('/', require('./error'));
