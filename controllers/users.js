159d4c507f087f9a7c920f454c13e7ead6807c41
Konstantin Filyaev <uzornakovre@yandex.ru>
2023-03-10 16:53:39 +0300

    fix: jwt

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,6 @@
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const { JWT_SECRET } = require('../config');
 const User = require('../models/user');
 const {
   OK,
@@ -98,7 +99,7 @@ module.exports.login = (req, res) => {
 
   return User.findUserByCredentials(email, password)
     .then((user) => {
-      const token = jwt.sign({ _id: user._id }, 'some-secret-key', { expiresIn: '7d' });
+      const token = jwt.sign({ _id: user._id }, JWT_SECRET, { expiresIn: '7d' });
       res.status(OK.CODE).send({ token });
     })
     .catch((err) => {
