973b013d45529f6f9e1281a2ae013f661ed1d94f
Konstantin Filyaev <uzornakovre@yandex.ru>
2023-03-10 22:12:23 +0300

    fix: port changed

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 const { JWT_SECRET = 'JWT_SECRET' } = process.env;
-const { PORT = '3001' } = process.env;
+const { PORT = '3000' } = process.env;
 const { DB_ADDRESS = 'mongodb://localhost:27017/mestodb' } = process.env;
 
 module.exports = {
