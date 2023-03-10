0b6d3a97af59bb94c01877de0c8d5abb5d218781
Konstantin Filyaev <uzornakovre@yandex.ru>
2023-03-10 22:59:13 +0300

    feat: joi added

--- routes/signup.js
+++ routes/signup.js
@@ -1,6 +1,15 @@
 const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
 const { createUser } = require('../controllers/users');
 
-router.post('/', createUser);
+router.post('/', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().email().required(),
+    password: Joi.string().required().min(4),
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string().uri().min(2),
+  }),
+}), createUser);
 
 module.exports = router;
