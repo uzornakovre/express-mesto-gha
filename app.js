8f1084478bcbbbdccdc32e31acc040d550499484
Konstantin Filyaev <uzornakovre@yandex.ru>
2023-03-10 16:10:34 +0300

    feat: env config

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ const mongoose = require('mongoose');
 const router = require('./routes/index');
 const errorHandler = require('./middlewares/errorHandler');
 
-const { PORT = 3000 } = process.env;
+const { PORT, DB_ADDRESS } = require('./config');
 
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/mestodb');
+mongoose.connect(DB_ADDRESS);
 
 app.use(express.json());
 app.use((req, res, next) => {
