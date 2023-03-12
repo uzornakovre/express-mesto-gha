274bc1804ecff4391113ca84cc1de44b63f859ba
Konstantin Filyaev <uzornakovre@yandex.ru>
2023-03-12 04:37:10 +0300

    fix: validation rules

--- routes/signup.js
+++ routes/signup.js
@@ -9,9 +9,7 @@ router.post('/', celebrate({
     password: Joi.string().required().min(4),
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
-    avatar: Joi.string().uri({
-      scheme: [urlValid],
-    }).min(2),
+    avatar: Joi.string().pattern(urlValid),
   }),
 }), createUser);
 
--- utils/validationRules.js
+++ utils/validationRules.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-useless-escape */
 
 // регулярное выражение для валидации URL
-const urlValid = /(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w\.-]*)*\/?/gi;
+const urlValid = /(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w\.-]*)*\/?/i;
 
 module.exports = {
   urlValid,
