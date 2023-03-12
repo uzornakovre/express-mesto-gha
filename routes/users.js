3f5dbcc117206abfe87e8b02167ea2ca7afb2f0c
Konstantin Filyaev <uzornakovre@yandex.ru>
2023-03-12 04:48:31 +0300

    fix: validation rules

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,6 @@
 const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
+const { urlValid } = require('../utils/validationRules');
 const auth = require('../middlewares/auth');
 const {
   getUsers,
@@ -12,7 +14,16 @@ router.use(auth);
 router.get('/', getUsers);
 router.get('/me', getCurrentUser);
 router.get('/:userId', getUser);
-router.patch('/me', updateUserInfo);
-router.patch('/me/avatar', updateUserAvatar);
+router.patch('/me', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+  }),
+}), updateUserInfo);
+router.patch('/me/avatar', celebrate({
+  body: Joi.object().keys({
+    avatar: Joi.string().pattern(urlValid),
+  }),
+}), updateUserAvatar);
 
 module.exports = router;
