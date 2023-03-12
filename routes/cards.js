b34ff62d4e82a61a4d8c06ad7e65581b064e14c6
Konstantin Filyaev <uzornakovre@yandex.ru>
2023-03-12 15:45:57 +0300

    fix: validation rules

--- routes/cards.js
+++ routes/cards.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const { celebrate, Joi } = require('celebrate');
-const { regexUrl } = require('../utils/validationRules');
+const { regexUrl, regexId } = require('../utils/validationRules');
 const auth = require('../middlewares/auth');
 const {
   getCards,
@@ -14,13 +14,25 @@ router.get('/', getCards);
 router.use(auth);
 router.post('/', celebrate({
   body: Joi.object().keys({
-    name: Joi.string().min(2).max(30),
-    link: Joi.string().pattern(regexUrl),
+    name: Joi.string().required().min(2).max(30),
+    link: Joi.string().required().pattern(regexUrl),
     createdAt: Joi.date(),
   }),
 }), createCard);
-router.delete('/:cardId', deleteCard);
-router.put('/:cardId/likes', likeCard);
-router.delete('/:cardId/likes', dislikeCard);
+router.delete('/:cardId', celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().pattern(regexId),
+  }),
+}), deleteCard);
+router.put('/:cardId/likes', celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().pattern(regexId),
+  }),
+}), likeCard);
+router.delete('/:cardId/likes', celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().pattern(regexId),
+  }),
+}), dislikeCard);
 
 module.exports = router;
